57f54c2766f432e256ed48560b39d13cbdd68c59
Andreas Stenius <andreas.stenius@svenskaspel.se>
2025-06-18 10:59:27 +0200

    Initial version, v0.1.

--- src/podsmith/__init__.py
+++ src/podsmith/__init__.py
@@ -3,7 +3,7 @@
 # See the LICENSE file for details.
 """**Podsmith** is a Python toolkit for managing Kubernetes-based test dependencies, enabling dynamic or pre-provisioned environments for integration testing."""
 
-__version__ = "1.0.0"
+__version__ = "0.1"
 
 from .config_map import ConfigMap
 from .manifest import random_text
